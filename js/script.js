fb35bf137a876eb6bf3d64cc8f89ab6d50d59e7e
sany0chekk <olexandr.veseluy@gmail.com>
2024-01-24 23:33:15 +0200

    v0.0.1

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
-const modal = document.querySelector("[data-modal]");
-const modalOpenBtn = document.querySelector("[data-modal-open]");
-const modalCloseBtn = document.querySelector("[data-modal-close]");
+// const modal = document.querySelector('[data-modal]');
+// const modalOpenBtn = document.querySelector('[data-modal-open]');
+// const modalCloseBtn = document.querySelector('[data-modal-close]');
 
-modalOpenBtn.addEventListener('click', () => {
-    modal.classList.add("is-open");
-    console.log("test")
-})
+// modalOpenBtn.addEventListener('click', () => {
+// 	modal.classList.add('is-open');
+// });
 
-modalCloseBtn.addEventListener('click', () => {
-    modal.classList.remove("is-open");
-})
+// modalCloseBtn.addEventListener('click', () => {
+// 	modal.classList.remove('is-open');
+// });
+
+const burger = document.querySelector('[data-burger]');
+const burgerOpenBtn = document.querySelector('[data-burger-open]');
+const burgerCloseBtn = document.querySelector('[data-burger-close]');
+
+burgerOpenBtn.addEventListener('click', () => {
+	burger.classList.add('burger-open');
+});
+
+burgerCloseBtn.addEventListener('click', () => {
+	burger.classList.remove('burger-open');
+});
