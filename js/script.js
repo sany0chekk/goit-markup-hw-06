7972f0c1e0d19e439e851235a691f5f31a821e44
sany0chekk <olexandr.veseluy@gmail.com>
2024-01-26 18:46:07 +0200

    v0.0.3

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-// const modal = document.querySelector('[data-modal]');
-// const modalOpenBtn = document.querySelector('[data-modal-open]');
-// const modalCloseBtn = document.querySelector('[data-modal-close]');
+const modal = document.querySelector('[data-modal]');
+const modalOpenBtn = document.querySelector('[data-modal-open]');
+const modalCloseBtn = document.querySelector('[data-modal-close]');
 
-// modalOpenBtn.addEventListener('click', () => {
-// 	modal.classList.add('is-open');
-// });
+modalOpenBtn.addEventListener('click', () => {
+	modal.classList.add('is-open');
+	document.body.style.overflow = 'hidden';
+});
 
-// modalCloseBtn.addEventListener('click', () => {
-// 	modal.classList.remove('is-open');
-// });
+modalCloseBtn.addEventListener('click', () => {
+	modal.classList.remove('is-open');
+	document.body.style.overflow = 'auto';
+});
 
 const burger = document.querySelector('[data-burger]');
 const burgerOpenBtn = document.querySelector('[data-burger-open]');
@@ -16,8 +18,10 @@ const burgerCloseBtn = document.querySelector('[data-burger-close]');
 
 burgerOpenBtn.addEventListener('click', () => {
 	burger.classList.add('burger-open');
+	document.body.style.overflow = 'hidden';
 });
 
 burgerCloseBtn.addEventListener('click', () => {
 	burger.classList.remove('burger-open');
+	document.body.style.overflow = 'auto';
 });
